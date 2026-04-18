3a46edbe91aed1aa1639d475f9b9560aa9ac1beb
Dionne <dionne.fernandes1@gmail.com>
2026-04-18 01:55:06 -0300

    feat: criação automática de superuser

---
+++ vendas_project/vendas/management/commands/createsu.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = 'Cria super usuário automaticamente'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(self.style.SUCCESS('Superusuário criado com sucesso'))
+        else:
+            self.stdout.write('Superusuário já existe')
