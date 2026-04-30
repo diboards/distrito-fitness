4e886c03ff0b2b76e1814518a13646c91ef8d9a0
Dionne <dionne.fernandes1@gmail.com>
2026-04-30 20:28:49 -0300

    fix: migration status_pagamento e status_entrega

--- vendas/migrations/0002_auto_20260430_2027.py
+++ vendas/migrations/0002_auto_20260430_2027.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('vendas', 'ULTIMA_MIGRATION_AQUI'),
+        ('vendas', '0001_initial'),  # 👈 USE O QUE EXISTE AQUI
     ]
 
     operations = [
