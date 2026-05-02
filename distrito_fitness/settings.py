c998fe40eb634892dedcaeabf91330c933f06b68
Dionne <dionne.fernandes1@gmail.com>
2026-05-02 13:33:12 -0300

    config banco + ajustes

--- distrito_fitness/settings.py
+++ distrito_fitness/settings.py
@@ -92,9 +92,9 @@ WSGI_APPLICATION = 'distrito_fitness.wsgi.application'
 # 🗄️ Banco (NEON)
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False
     )
 }
 
