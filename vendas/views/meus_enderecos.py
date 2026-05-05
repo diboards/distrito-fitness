0f5e79e9d3af40145b68d880fab7a66100f0b1de
diboards <155734323+diboards@users.noreply.github.com>
2026-05-05 16:27:32 -0300

    Update meus_enderecos.py

--- vendas/views/meus_enderecos.py
+++ vendas/views/meus_enderecos.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
-from .models import Endereco
-from .forms import EnderecoForm
+from ..models import Endereco
+from ..forms import EnderecoForm
 
 
 @login_required
