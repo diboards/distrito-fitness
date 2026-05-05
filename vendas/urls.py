ffacd867b817cc6484ce8d9f08b5acc3b4ac1367
diboards <155734323+diboards@users.noreply.github.com>
2026-05-05 16:44:23 -0300

    Update urls.py

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,6 +1,6 @@
 # vendas/urls.py
 from django.urls import path
-from . import views
+from ..import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
