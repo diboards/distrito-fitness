0dead1bce34f249c7ebbd100174caa8815b8783a
diboards <155734323+diboards@users.noreply.github.com>
2026-05-05 17:03:26 -0300

    Update __init__.py

--- vendas/views/__init__.py
+++ vendas/views/__init__.py
@@ -2,20 +2,7 @@
 # vendas/views/__init__.py
 
 # Importa do views.py principal
-from .views import (
-    pagina_inicial,
-    estoque,
-    lista_vendas,
-    cadastrar_produto,
-    nova_venda,
-    relatorios,
-    editar_venda,
-    atualizar_venda,
-    login_view,
-    registrar_com_endereco,
-    registrar_usuario,
-    solicitar_orcamento,
-)
+from .views import *
 
 # Importa dos outros módulos
 from .meu_perfil import *
