d34f33e33a34342009b440848477dbc13c9c5fbc
diboards <155734323+diboards@users.noreply.github.com>
2026-05-06 00:34:24 -0300

    Update meu_perfil.py

--- vendas/views/meu_perfil.py
+++ vendas/views/meu_perfil.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
-from models import Perfil
 from forms import PerfilForm
 from vendas.models import Perfil  # ou from ..models import Perfil
 
