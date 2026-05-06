af2147753de3f8f1557c41072996ebb3b37833eb
diboards <155734323+diboards@users.noreply.github.com>
2026-05-06 00:24:43 -0300

    Update urls.py

--- vendas/urls.py
+++ vendas/urls.py
@@ -4,7 +4,7 @@ from .import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from .meu_perfil import meu_perfil  # Importa de arquivo separado
+from vendas.views import meu_perfil  # ← Importa da pasta views
 
 urlpatterns = [
     path('', views.pagina_inicial, name='pagina_inicial'),
