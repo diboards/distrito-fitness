c02128607ed16b8b46abc0527e2a6d6315f1a435
diboards <155734323+diboards@users.noreply.github.com>
2026-06-08 16:03:18 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,19 @@
+
+# manage.py
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import subprocess
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'distrito_fitness.settings')
+    
+    # Executar migrações automaticamente se estiver no Render
+    if os.environ.get('RENDER'):
+        print("🔄 Executando migrações...")
+        subprocess.run([sys.executable, "manage.py", "makemigrations", "vendas", "--noinput"])
+        subprocess.run([sys.executable, "manage.py", "migrate", "--noinput"])
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +23,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
