6c8da075b07908aa6fd9519cb18f2e08a48ca354
diboards <155734323+diboards@users.noreply.github.com>
2026-06-09 16:00:21 -0300

    Update and rename 0005_add_imagem_column.py.py to 0005_add_imagem_column.py.py.py

---
+++ vendas/migrations/0005_add_imagem_column.py.py.py
@@ -0,0 +1,15 @@
+# vendas/migrations/0005_add_imagem_column.py
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('vendas', '0004_rename_imagem_selecionada_carrinhoitem_imagem'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='produto',
+            name='imagem',
+            field=models.CharField(blank=True, max_length=200, null=True),
+        ),
+    ]
