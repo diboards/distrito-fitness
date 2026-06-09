c048f02e3f57a796e7df148f97fe7fd03491ada0
diboards <155734323+diboards@users.noreply.github.com>
2026-06-09 13:29:03 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,17 @@ echo "========================================="
 # Instalar dependências
 pip install -r requirements.txt
 
-# Criar e aplicar migrações
+# Remover migrações antigas que podem estar causando conflito
+rm -f vendas/migrations/00*.py
+
+# Criar novas migrações
 python manage.py makemigrations vendas --noinput
+
+# Forçar a migração (ignorando erros de coluna)
+python manage.py migrate vendas --noinput --fake-initial
+
+# Criar e aplicar migrações para o resto
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 # Coletar arquivos estáticos
