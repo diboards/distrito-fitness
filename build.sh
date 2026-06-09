0efc4a08d0bc8c6bfca8add4e952c8cede7a0284
diboards <155734323+diboards@users.noreply.github.com>
2026-06-09 16:22:28 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ echo "========================================="
 
 pip install -r requirements.txt
 
-# Criar e aplicar migrações
-python manage.py makemigrations vendas --noinput
+# Aplicar migrações
 python manage.py migrate vendas --noinput
 python manage.py collectstatic --noinput
 
