8325dbdb7bc3bddebc8bcd5479d76d4d53edd0a7
diboards <155734323+diboards@users.noreply.github.com>
2026-06-09 15:41:55 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,8 +10,21 @@ pip install -r requirements.txt
 # Criar migrações
 python manage.py makemigrations vendas --noinput
 
-# Aplicar migrações (incluindo a nova)
+# Aplicar todas as migrações pendentes
 python manage.py migrate vendas --noinput
+
+# Se ainda tiver erro, renomear a coluna manualmente via SQL
+python manage.py dbshell << EOF
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='vendas_produto' AND column_name='imagem_principal') THEN
+        ALTER TABLE vendas_produto RENAME COLUMN imagem_principal TO imagem;
+        RAISE NOTICE 'Coluna renomeada com sucesso';
+    ELSE
+        RAISE NOTICE 'Coluna imagem_principal não existe';
+    END IF;
+END $$;
+EOF
 
 # Coletar arquivos estáticos
 python manage.py collectstatic --noinput
