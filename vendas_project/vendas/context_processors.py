db03e7bf935605a5c9516a7a6f5568c56e6e5d49
Dionne <dionne.fernandes1@gmail.com>
2026-07-15 14:44:09 -0300

    Corrige models.py com null=True e atualiza scripts de migração

--- vendas_project/vendas/context_processors.py
+++
@@ -1,7 +0,0 @@
-# vendas/context_processors.py
-from django.conf import settings
-
-def mercadopago_settings(request):
-    return {
-        'MERCADOPAGO_PUBLIC_KEY': getattr(settings, 'MERCADOPAGO_PUBLIC_KEY', ''),
-    }
