0f87228270ff7ca4858fd99a60d0fdc917b47840
diboards <155734323+diboards@users.noreply.github.com>
2026-07-16 00:18:59 -0300

    Update urls.py

--- distrito-fitness/distrito_fitness/urls.py
+++ distrito-fitness/distrito_fitness/urls.py
@@ -18,11 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def teste(request):
+    return HttpResponse("Teste funcionando!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('teste/', teste),  # ← ROTA DE TESTE
     path('', include('vendas.urls')),
 ]
+
 
 # SERVIR MEDIA SEMPRE (inclusive produção)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
