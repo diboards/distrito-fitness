721940930f506b228499a6fdc7391b2dc4228beb
diboards <155734323+diboards@users.noreply.github.com>
2026-08-16 16:49:57 -0300

    Update urls.py

--- distrito-fitness/vendas/urls.py
+++ distrito-fitness/vendas/urls.py
@@ -40,6 +40,8 @@ urlpatterns = [
     # Painel
     path('painel/pedidos/', views_principais.gerenciar_pedidos, name='gerenciar_pedidos'),
     #path('painel/pedido/<int:pedido_id>/status/', views_principais.atualizar_status_entrega, name='atualizar_status_entrega'),
+    # 🔥 ADMIN - ATUALIZAR STATUS MANUALMENTE
+    path('admin/pedido/<int:pedido_id>/status/', views.admin_atualizar_status, name='admin_atualizar_status'),
     
     # Carrinho e produtos
     path('produto/<int:produto_id>/', views_principais.detalhes_produto, name='detalhes_produto'),
